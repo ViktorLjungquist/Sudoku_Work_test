2e00b2caaefb4ee6eeaf4c7d5623e60a70e9e393
viktor <viktor.ljungquist@amendum.se>
2022-03-27 17:37:55 +0200

    first test of developed code

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "public/index.css";
+import Board from "./sudokuGenerator.js";
+import Menu from "./Menu.js";
+import "bootstrap/dist/css/bootstrap.css";
+var _ = require("lodash");
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initial: 33,
+    };
+  }
+  handleGeneration = (initial) => {
+    this.setState({ initial: initial });
+  };
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board key={_.random(0, 10000)} initial={this.state.initial} />
+          <div className="game-menu">
+            <Menu onGenerate={this.handleGeneration} />
+          </div>
+        </div>
+        <a
+          className="github-link"
+          href="https://github.com/ViktorLjungquist/Sudoku_Work_test"
+        >
+          View Code on Github.
+        </a>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
